45cac7f48afc2dc3b66a020067afc84a6d127c49
Pedro H Meira <pedrohmeirasilva@gmail.com>
2023-07-27 22:24:06 -0300

    Create Script for LevelPiece

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -7,13 +7,27 @@ public class LevelManager : MonoBehaviour
     public Transform container;
 
     public List<GameObject> levels;
+
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+    public int piecesStartNumber = 3;
+    public int piecesNumber = 5;
+    public int piecesEndNumber = 1;
+
+    public float timeBetweenPieces = .3f;
     
     [SerializeField] private int _index;
     private GameObject _currentLevel;
 
+
+    public List<LevelPieceBase> _spawnedPieces;
+
     private void Awake()
     {
-        SpawnNextlevel();
+        //SpawnNextlevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextlevel()
@@ -36,6 +50,54 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+
+    #region
+    private void CreateLevelPieces()
+    {
+        StartCoroutine(CreateLevelPiecesCoroutine());
+    }
+
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count-1];
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+
+        _spawnedPieces.Add( spawnedPiece);
+    }
+
+    IEnumerator CreateLevelPiecesCoroutine()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+         
+
+        for (int i = 0; i < piecesStartNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesStart);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece(levelPieces);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+        
+        for (int i = 0; i < piecesEndNumber; i++)
+        {
+            CreateLevelPiece(levelPiecesEnd);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+
+    }
+    #endregion
+
 
     private void Update() 
     {
